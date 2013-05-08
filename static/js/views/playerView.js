ca181b1bb4d7ad9b9520215490abbc0157702de0
Drew Harris <drew@drews-mac-pro.(none)>
2013-05-08 16:53:07 -0700

    more updating

--- static/js/views/playerView.js
+++ static/js/views/playerView.js
@@ -6,17 +6,17 @@ var PlayerView = Backbone.View.extend({
     initialize: function(){
         var html = this.template();
         this.$el.append(html);
-        this.canvas = $("#loading-canvas")[0];
-        this.context = this.canvas.getContext('2d');
-        this.loadingImage = new Image();
-        this.loadingImage.src = '/static/images/player/load_50_w.png';
-        this.counter = 0;
-        this.TO_RADIANS = Math.PI/180;
-        this.rotateTimer = null;
-        this.loadingImage.onload = function(){
-            this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
-            this.drawRotatedImage(this.loadingImage,this.canvas.width/2,this.canvas.height/2,0);
-        }.bind(this);
+        // this.canvas = $("#loading-canvas")[0];
+        // this.context = this.canvas.getContext('2d');
+        // this.loadingImage = new Image();
+        // this.loadingImage.src = '/static/images/player/load_50_w.png';
+        // this.counter = 0;
+        // this.TO_RADIANS = Math.PI/180;
+        // this.rotateTimer = null;
+        // this.loadingImage.onload = function(){
+            // this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
+            // this.drawRotatedImage(this.loadingImage,this.canvas.width/2,this.canvas.height/2,0);
+        // }.bind(this);
     },
     events: {
         'click #play-image': 'emitPlay',
@@ -40,34 +40,34 @@ var PlayerView = Backbone.View.extend({
 
     },
     startLoading: function(){
-        $("#loading").show();
-        this.resetLoading();
-        this.rotateTimer = setInterval(function(){
-            this.animate();
-        }.bind(this), 1000/60);
+        // $("#loading").show();
+        // this.resetLoading();
+        // this.rotateTimer = setInterval(function(){
+            // this.animate();
+        // }.bind(this), 1000/60);
     },
     stopLoading: function(){
-        $("#loading").hide();
-        this.resetLoading();
+        // $("#loading").hide();
+        // this.resetLoading();
     },
     resetLoading: function(){
-        clearInterval(this.rotateTimer);
-        delete this.rotateTimer;
+        // clearInterval(this.rotateTimer);
+        // delete this.rotateTimer;
     },
     animate: function(){
-        this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
-        this.drawRotatedImage(this.loadingImage,
-            this.canvas.width/2,
-            this.canvas.height/2,
-            this.counter);
-        this.counter+=5;
+        // this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
+        // this.drawRotatedImage(this.loadingImage,
+            // this.canvas.width/2,
+            // this.canvas.height/2,
+            // this.counter);
+        // this.counter+=5;
     },
     drawRotatedImage: function(image, x, y, angle) {
-        this.context.save();
-        this.context.translate(x, y);
-        this.context.rotate(angle * this.TO_RADIANS);
-        this.context.drawImage(image, -(image.width/2), -(image.height/2));
-        this.context.restore();
+        // this.context.save();
+        // this.context.translate(x, y);
+        // this.context.rotate(angle * this.TO_RADIANS);
+        // this.context.drawImage(image, -(image.width/2), -(image.height/2));
+        // this.context.restore();
     },
     showPause: function(){
         $("#play-image").hide();
